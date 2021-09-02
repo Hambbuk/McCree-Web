cf1c94d69bc07cff14e18eba56621c41a949ee8c
Hanbin Bae <HANBIN1931@GMAIL.COM>
2021-09-02 18:46:10 +0900

    init: 테스트 페이지

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button, Progress} from 'semantic-ui-react'
+import imgA from './images/001.png';
+
+function App() {
+  return (
+    <div>
+<p>아래 사진이 뭔가욤?</p>
+    <div>
+<img
+   src={ imgA }
+   width='400'
+   height='300'
+   alt='testA' />
+</div>
+<Button primary> 곰 </Button>
+<Button primary> 고양이 </Button>
+<Button primary> 강아지 </Button>
+<Button primary> 고래 </Button>
+<Progress percent = {66} />
+</div>
+  );
+}
+
+export default App;
